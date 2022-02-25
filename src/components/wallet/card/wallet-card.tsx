d6a2305fb20ac73c4c433cc8341701f2e906fece
maryia-deriv <maryia@deriv.com>
2022-02-25 14:22:46 +0300

    Maryia/option with gradient

--- src/components/wallet/card/wallet-card.tsx
+++ src/components/wallet/card/wallet-card.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
 import classNames from 'classnames';
 import css from './wallet-card.module.scss';
-import DefaultLightBackground from '@assets/svg/card/default-light-background.svg';
-import DefaultDarkBackground from '@assets/svg/card/default-dark-background.svg';
 import LogoPlaceholderLight from '@assets/svg/card/ic-logo-placeholder-light.svg';
 import LogoPlaceholderDark from '@assets/svg/card/ic-logo-placeholder-dark.svg';
 
 export const wallet_card_sizes = ['small', 'medium', 'large'];
 
 export interface WalletCardProps {
-    background_image?: string;
     balance?: string;
     currency?: string;
     dark?: boolean;
@@ -19,18 +15,9 @@ export interface WalletCardProps {
     wallet_name?: string;
 }
 
-const WalletCard = ({
-    background_image,
-    balance,
-    currency,
-    dark,
-    faded,
-    logo,
-    size = 'large',
-    wallet_name,
-}: WalletCardProps) => {
-    const [is_hovered, setIsHovered] = useState(false);
-    const background = background_image || (dark ? DefaultDarkBackground : DefaultLightBackground);
+// TODO: active state, selection of logos based on the wallet_name
+
+const WalletCard = ({ balance, currency, dark, faded, logo, size = 'large', wallet_name }: WalletCardProps) => {
     const payment_method_logo = logo || (dark ? LogoPlaceholderDark : LogoPlaceholderLight);
 
     const getCardInfo = () => {
@@ -52,20 +39,13 @@ const WalletCard = ({
 
     return (
         <div
-            className={classNames(css[size], css.container, dark && css.dark, faded && css.faded)}
-            style={{
-                background: `${
-                    is_hovered && !faded
-                        ? `linear-gradient(180deg, ${
-                              dark
-                                  ? 'rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0.16)'
-                                  : 'rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.16)'
-                          } 100%), `
-                        : ''
-                }url(${background})`,
-            }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            className={classNames(
+                css[size],
+                css.container,
+                wallet_name && wallet_name !== '[Name]' ? css[wallet_name.replaceAll(' ', '_').toLowerCase()] : css.background__default,
+                dark && css.dark,
+                faded && css.faded,
+            )}
         >
             <img className={css.logo} src={payment_method_logo} alt={'payment_method_logo'} />
             <div>{getCardInfo()}</div>
